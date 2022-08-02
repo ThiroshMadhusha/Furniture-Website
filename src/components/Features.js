a8c09efc7cff5406ec48cc96ae1e2cc9bd1d226a
Thirosh Madhusha <thiroshmadhusha0520@gmail.com>
2022-08-02 22:03:17 +0530

    Created Feature option section

--- src/components/Features.js
+++ src/components/Features.js
@@ -1,7 +1,44 @@
-import React from 'react';
+import React from "react";
+
+// import data
+import { features } from "../data";
 
 const Features = () => {
-  return <div>Features</div>;
+  // destructure feactures
+  const { title, subtitle, image, buttonText, items } = features;
+  return (
+    <section className="section">
+      <div className="container mx-auto">
+        <div className="flex flex-col lg:flex-row lg:gap-x-[100px]">
+          {/* Images */}
+          <div className="flex-1 order-1 lg:-order-1">
+            <img src={image.type} alt="" />
+          </div>
+          {/* Text */}
+          <div className="flex-1 flex flex-col justify-end">
+            <h2 className="title">{title}</h2>
+            <p className="subtitle">{subtitle}</p>
+
+            {/* Items */}
+            <div>
+              {items.map((item, index) => {
+                const { title, subtitle, icon } = item;
+                return (
+                  <div className="flex mb-6 lg:last:mb-0">
+                    <div className="text-2xl mr-4 lg:text-3xl">{icon}</div>
+                    <div className="text-base lg:text-x1 font-semibold">
+                      <h4>{title}</h4>
+                      <p>{subtitle}</p>
+                    </div>
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Features;
--- src/components/FeaturesSecond.js
+++ src/components/FeaturesSecond.js
@@ -1,7 +1,26 @@
-import React from 'react';
+import React from "react";
+// import data
+import { features } from "../data";
 
 const FeaturesSecond = () => {
-  return <div>FeaturesSecond</div>;
+  const { title, subtitle, image } = features.feature2;
+  return (
+    <section className="section">
+      <div className="container mx-auto">
+        <div className="flex flex-col lg:flex-row lg:gap-x-[100px]">
+          {/* Text */}
+          <div className="flex-1 flex flex-col justify-end">
+            <h2 className="title">{title}</h2>
+            <p className="subtitle">{subtitle}</p>
+          </div>
+          {/* Images */}
+          <div className="flex-1 order-1 lg:-order-1">
+            <img src={image.type} alt="" />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default FeaturesSecond;
