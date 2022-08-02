0a396cd7716b20e19380ff24e0d6abf50dbf84bc
Thirosh Madhusha <thiroshmadhusha0520@gmail.com>
2022-08-02 22:05:44 +0530

    Complete Footer Section

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,40 @@
-import React from 'react';
+import React from "react";
+
+// import logo
+import Logo from "../assets/img/logo.svg";
+
+// import daa
+import { footer } from "../data";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className="section bg-primary text-white">
+      <div className="container mx-auto">
+        <div className="flex flex-col lg:flex-row justify-between border-b border-opacity-75 border-gray-700 pb-7 lg:pb-14 mb-14">
+          <a href="#link" className="mb-6 lg:mb-0">
+            <img src={Logo} alt="" />
+          </a>
+          {/* Socials */}
+          <div className="flex gap-x-4">
+            {footer.social.map((item, index) => {
+              return (
+                <div
+                  className="w-12 h-12 text-2xl bg-gray-700 hover:bg-accent rounded-full flex justify-center items-center transition-all"
+                  key={index}
+                >
+                  <a href="#icon">{item.icon}</a>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+        {/* caopyright */}
+        <p className="text-center">
+          &copy;Thirosh Madhusha 2022 - All rights reserved
+        </p>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
