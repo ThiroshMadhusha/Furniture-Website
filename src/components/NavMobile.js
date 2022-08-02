6e23659036997edc1c1eeef1182a7e80cfd70cc8
Thirosh Madhusha <thiroshmadhusha0520@gmail.com>
2022-08-02 22:03:43 +0530

    Created Header and Response nav bar section

--- src/components/NavMobile.js
+++ src/components/NavMobile.js
@@ -1,7 +1,20 @@
 import React from 'react';
 
+import { navigation } from '../data';
+
 const NavMobile = () => {
-  return <div>NavMobile</div>;
+  return <div className='bg-white w-full h-full shadow-2xl'>
+    <ul className='text-center h-full flex flex-col items-center justify-center gap-y-6'>
+      {navigation.map((item, index) => {
+        return (
+          <li key={index}>
+            <a className='text-xl font-medium capitalize' href={item.href}>{item.name}</a>
+          </li>
+        )
+      })}
+    </ul>
+    
+  </div>;
 };
 
 export default NavMobile;
