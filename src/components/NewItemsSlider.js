82372f7d6cfb52927c4a314626103e6ca8fa01d0
Thirosh Madhusha <thiroshmadhusha0520@gmail.com>
2022-08-02 22:05:33 +0530

    Create New Items Section

--- src/components/NewItemsSlider.js
+++ src/components/NewItemsSlider.js
@@ -1,7 +1,42 @@
-import React from 'react';
+import React from "react";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+// import data
+import { newInStore } from "../data";
 
 const NewItemsSlider = () => {
-  return <div>NewItemsSlider</div>;
+  return (
+    <Swiper
+      grabCursor={true}
+      breakpoints={{
+        320: {
+          slidesPerView: 2,
+          spaceBetween: 18,
+        },
+        768: {
+          slidesPerView: 3,
+          spaceBetween: 20,
+        },
+      }}
+      className="mySwiper"
+    >
+      {newInStore.products.map((product, index) => {
+        return (
+          <SwiperSlide className="max-w-[265px]" key={index}>
+            <div className="relative">
+              <img src={product.image.type} alt="" />
+              <div className="absolute text-white bottom-[20px] text-center w-full text-[18px] lg:text-2xl font-medium capitalize">
+                {product.name}
+              </div>
+            </div>
+          </SwiperSlide>
+        );
+      })}
+    </Swiper>
+  );
 };
 
 export default NewItemsSlider;
