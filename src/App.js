cf7c5abbdad0eb0baabfe606aaedc098e341ecd5
Thirosh Madhusha <thiroshmadhusha0520@gmail.com>
2022-08-02 22:07:26 +0530

    Add all Main Components Part In to App.js File

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
-import React from 'react';
+import React from "react";
 
 // import components
+import Hero from "./components/Hero";
+import Feactures from "./components/Features";
+import NewItems from "./components/NewItems";
+import FeaturesSecond from "./components/FeaturesSecond";
+import Products from "./components/Products";
+import Testimonials from "./components/Testimonial";
+import Newsletter from "./components/Newsletter";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
 
 const App = () => {
-  return <div>react app</div>;
+  return (
+    <div className="w-full max-w-[1440px] mx-auto bg-white">
+      <Header />
+      <Hero />
+      <Feactures />
+      <NewItems />
+      <FeaturesSecond />
+      <Products />
+      <Testimonials />
+      <Newsletter />
+      <Footer />
+    </div>
+  );
 };
 
 export default App;
