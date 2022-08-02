047d9b124ba35d16994fa94ec0932d308fda329f
Thirosh Madhusha <thiroshmadhusha0520@gmail.com>
2022-08-02 22:05:17 +0530

    Add Products Details Components

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,7 +1,23 @@
-import React from 'react';
+import React from "react";
+
+// import data
+import { products } from "../data";
+
+// import components
+import ProductSlider from "./ProductSlider";
 
 const Products = () => {
-  return <div>Products</div>;
+  // destructure data
+  const { title, subtitle } = products;
+  return <section className="section text-center">
+    <div className="container mx-auto">
+      <div>
+        <h2 className="title">{title}</h2>
+        <p className="max-w-[639px] mx-auto mb-[50px] lg:mb-[70px]">{subtitle}</p>
+      </div>
+      <ProductSlider/>
+    </div>
+  </section>;
 };
 
 export default Products;
