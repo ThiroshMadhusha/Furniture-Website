e932b837173d0ab3f4e8f43bfd77a0a906494278
Thirosh Madhusha <thiroshmadhusha0520@gmail.com>
2022-08-02 22:04:12 +0530

    Implement Testimonials section

--- src/components/TestimonialSlider.js
+++ src/components/TestimonialSlider.js
@@ -1,7 +1,47 @@
-import React from 'react';
+import React from "react";
+
+// import swiper
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// import swiper styles
+import "swiper/css";
+import "swiper/css/navigation";
+
+// import required modules
+import { Autoplay, Navigation } from "swiper";
+
+// import data
+import { testimonial } from "../data";
 
 const TestimonialSlider = () => {
-  return <div>TestimonialSlider</div>;
+  return (
+    <Swiper
+      className="testimonialSlider"
+      navigation={true}
+      modules={[Navigation, Autoplay]}
+      autoplay={true}
+    >
+      {testimonial.persons.map((person, index) => {
+        const { avatar, name, occupation, message } = person;
+        return (
+          <SwiperSlide key={index}>
+            <div className="flex flex-col min-h-[250px]">
+              <div className="flex items-center gap-x-5 mb-9">
+                {/* Avatar */}
+                <img src={avatar.type} alt="" />
+                <div>
+                  <div className="text-xl font-semibold">{name}</div>
+                  <div className="text-gray-500">{occupation}</div>
+                </div>
+              </div>
+              {/* Texts */}
+              <div className="text-xl max-w-[570px]">{message}</div>
+            </div>
+          </SwiperSlide>
+        );
+      })}
+    </Swiper>
+  );
 };
 
 export default TestimonialSlider;
